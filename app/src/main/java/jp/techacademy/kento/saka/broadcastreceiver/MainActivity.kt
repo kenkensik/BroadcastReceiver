0a529f0cd91ded3642b9a17e465d8b9d4d678a1d
kenkensik <kenkensik@yahoo.co.jp>
2019-10-15 20:20:12 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/kento/saka/broadcastreceiver/MainActivity.kt
@@ -0,0 +1,17 @@
+package jp.techacademy.kento.saka.broadcastreceiver
+
+import android.content.BroadcastReceiver
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Context
+import android.widget.Toast
+import android.content.Intent
+
+
+class MainActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
